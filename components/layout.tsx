636cbb6e181540736a9ea61039e038d6ed2d3769
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-30 21:52:06 -0300

    Update OG Image

--- components/layout.tsx
+++ components/layout.tsx
@@ -25,7 +25,7 @@ export default function Layout({ children, home }: LayoutProps) {
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=dark&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
